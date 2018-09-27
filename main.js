d5fa6b321184a4750c2c4e0390cc3917173521cf
Hadar Abukrat <hadarabukrat@Chaim-As-MacBook.local>
2018-09-26 17:06:28 -0700

    added map to contact page

--- main.js
+++ main.js
@@ -26,3 +26,17 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
+
+$('#sendMessage').click(function(){
+	$('#sendMessage').hide();
+	$('#thankyou').show();
+});
+
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(34.050870, -118.237088),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
